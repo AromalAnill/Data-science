ad729bf64d2e252f4775d39a441013d43178f1c7
AromalAnill <aaromalanill@gmail.com>
2026-07-17 15:09:39 +0530

    Delete add.py

--- add.py
+++
@@ -1,8 +0,0 @@
-dividend = float(input("Enter the dividend: "))
-divisor = float(input("Enter the divisor: "))
-
-if divisor == 0:
-    print("Error: Division by zero is not allowed.")
-else:
-    result = dividend / divisor
-    print("Result =", result)
