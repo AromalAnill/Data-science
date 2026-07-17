2df6259788b25212f89d62f3e4fc9f8b059afe39
AromalAnill <aaromalanill@gmail.com>
2026-07-17 15:36:08 +0530

    Add files via upload

---
+++ creatdic.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import pandas as pd
+data={"Name":["paul","Raj","Reena","Anu","Amal"],
+      "Age":[75,34,25,16,15],
+      "city":["kottayam","alappuzha","erk","idukki","kozhikode"]}
+
+df=pd.DataFrame(data)
+print(df)
