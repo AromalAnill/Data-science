60951ae53e86984a1c742de32dbcbb63a9cb081f
AromalAnill <aaromalanill@gmail.com>
2026-07-17 15:10:07 +0530

    Delete dict.py

--- dict.py
+++
@@ -1,9 +0,0 @@
-student={"name":"archana","age":12,"course":"python"}
-print(student)
-print("name:",student["name"])
-student["collage"]="mangalam college of engineering"
-print(student)
-student["age"]=22
-print(student)
-student.pop("course")
-print("after removing",student)
